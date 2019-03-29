0adcb216e3f81d96601441711bd410067f8ecf9f
Too-Tao <599366020@qq.com>
2019-03-29 22:03:03 +0800

    eslint规范

--- day002/start_server/server.js
+++ day002/start_server/server.js
@@ -1,14 +1,17 @@
 var http = require('http')
 
 function start () {
-  function onRequest(request, response) {
-    console.log("Request received.")
-    response.writeHead(200,{"Content-Type": "text/plain"})
-    response.write('Hello World')
-    response.end()
-  }
-  http.createServer(onRequest).listen(8888)
-  console.log("Server has started")
+    function onRequest(request, response) {
+
+        // eslint-disable-next-line no-console
+        console.log('Request received.')
+        response.writeHead(200,{'Content-Type': 'text/plain'})
+        response.write('Hello World')
+        response.end()
+    }
+    http.createServer(onRequest).listen(8888)
+    // eslint-disable-next-line no-console
+    console.log(' Server has started ')
 }
 
 exports.start = start
