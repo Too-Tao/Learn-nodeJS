4a111fe0ae6e6240ba0555d59914aba3bbb76626
Too-Tao <599366020@qq.com>
2019-05-12 10:13:46 +0800

    add comment (#0002)

--- nodeBasic/001/module.js
+++ nodeBasic/001/module.js
@@ -1,1 +1,14 @@
+// function (exports, module, __filename, __dirname) {
+
+// }
+
+// exports
+//   - 用于将对象，函数暴露在外部
+// module
+//   - 代表模块本身
+// __filename
+//   - 当前模块完整路径
+// __dirname
+//   - 当前模块所在文件夹的位置
+
 console.log(exports)
